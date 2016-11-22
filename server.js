bffb15de97c89608725a449f7b2c4ad5ec2db1c2
swethamurali <swetha1695@gmail.com>
2016-11-22 15:12:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/page', function (req, res) {
-  res.send('this is my home');
+ res.sendFile(path.join(__dirname, 'ui', 'page.html'));
   
 });
 
