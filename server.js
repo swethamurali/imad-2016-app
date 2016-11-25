3a71ae4a0dabc868cf0f1b1a73c5d4359a736d02
swethamurali <swetha1695@gmail.com>
2016-11-25 21:47:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ app.get('/form', function (req, res) {
   
 });
 
+var service_id = 'my_mandrill';
+var template_id = 'feedback';
+var template_params = {
+name: 'John',
+reply_email: 'swetha1695@gmail.com',
+message: 'This is awesome!'
+};
+
+emailjs.send(service_id,template_id,template_params);
   
 
 
